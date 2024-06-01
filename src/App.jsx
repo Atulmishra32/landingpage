1375e27b318f234a46609544e98b02b8b77194cd
Aman Singh <119008813+Atulmishra32@users.noreply.github.com>
2024-06-01 14:21:55 +0530

    updated

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Body from "./Components/Body";
+function App() {
+  return (
+    <>
+      <div className="bg-amber-400">
+        <Header />
+        <Body />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
